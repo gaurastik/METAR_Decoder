422aff745491ab45ee42f84268b8df18e74f6be8
Roshan Karanth <roshankaranth@gmail.com>
2023-06-14 21:11:05 +0530

    Added error handling

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const weather_API = require('./utils/weather_api')
 
 
-weather_API.Weather_data((data) => {
-    console.log(data)
+weather_API.Weather_data((error, data) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(data)
+    }
 })
--- backend/utils/weather_api.js
+++ backend/utils/weather_api.js
@@ -5,9 +5,12 @@ const Weather_data = (callback) => {
 
     const url = 'https://api.checkwx.com/metar/VIDP,VABB,VOBL,VOHS,VOMM,VECC,VAAH,VOCI,VOGO,VAPO,VILK,VEGT,VIJP,VISR,VEPT,VICG,VEBS,VOTV,VOCL,VAID/decoded?x-api-key=20cd2ef316fb47789201db7202'
 
-
     request({ url: url, json: true }, (error, response) => {
-        callback(response.body)
+        if (error) {
+            callback(error)
+        } else {
+            callback(undefined, response.body)
+        }
     })
 }
 
