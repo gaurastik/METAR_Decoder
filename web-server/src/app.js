c8becb34a0eaba270ceded30b81332ad1fb7329a
Roshan Karanth <roshankaranth@gmail.com>
2023-06-18 21:44:42 +0530

    Connected frontend to web server

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
-
+const path = require('path')
 const app = express()
 
-app.get('', (req, res) => {
-    res.send("Hello Express!!")
-})
+const frontend_dir_path = path.join(__dirname, '../frontend+QGIS')
+
+app.use(express.static(frontend_dir_path))
+
 
 app.listen(3000, () => {
     console.log("Server is up and running!")
