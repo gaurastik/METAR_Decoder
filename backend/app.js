8ff3a7b4778aaf16c58e2c3f3c4aa18f50bc3bc0
Roshan Karanth <roshankaranth@gmail.com>
2023-06-24 18:48:33 +0530

    Created API call to fetch weather data of 73 Airports

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,22 @@
 const weather_API = require('./utils/weather_api')
+const fs = require('fs')
 
+const airport_query = [
+    'VOMM,VOCB,VOMD,VOPC,VOSM,VOTR,VOTK,VOBL,VOBG,VOML,VOMY,VOGB,VOJV,VOHS,VOCP,VOHY,VORY,VOTP,VOBZ,VOTV',
+    'VECC,VECO,VEDG,VEBD,VEBS,VEJH,VEPY,VEPT,VEGY,VEJS,VERC,VOKN,VOCL,VOCI,VETZ,VEAT,VELP,VEBI,VEIM,VELR',
+    'VEMR,VEMN,VASU,VAAH,VABO,VABV,VAKE,VAPR,VARK,VADU,VASD,VABB,VAAU,VAJL,VAJJ,VERP,VABP,VAID,VAJB,VIJR',
+    'VANP,VEBN,VIBR,VIGG,VISM,VIJU,VISR,VAUD,VILK,VIJP,VIDN,VIAR,VIDP']
 
-weather_API.Weather_data((error, data) => {
-    if (error) {
-        console.log(error)
-    } else {
-        console.log(data)
-    }
-})
+for (let i = 0; i < 4; i++) {
+    weather_API.Weather_data(airport_query[i], (error, data) => {
+        if (error) {
+            const error_json = JSON_stringify(error)
+            fs.writeFileSync('weather_data.json', error)
+
+        } else {
+            const data_json = JSON.stringify(data)
+            fs.appendFileSync(`weather_data.json`, data_json)
+
+        }
+    })
+}
--- backend/utils/weather_api.js
+++ backend/utils/weather_api.js
@@ -1,9 +1,8 @@
 const request = require('request')
 
+const Weather_data = (airport_query_i, callback) => {
 
-const Weather_data = (callback) => {
-
-    const url = 'https://api.checkwx.com/metar/VIDP,VABB,VOBL,VOHS,VOMM,VECC,VAAH,VOCI,VOGO,VAPO,VILK,VEGT,VIJP,VISR,VEPT,VICG,VEBS,VOTV,VOCL,VAID/decoded?x-api-key=20cd2ef316fb47789201db7202'
+    const url = `https://api.checkwx.com/metar/${airport_query_i}/decoded?x-api-key=20cd2ef316fb47789201db7202`
 
     request({ url: url, json: true }, (error, response) => {
         if (error) {
