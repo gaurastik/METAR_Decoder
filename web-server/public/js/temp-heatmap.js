f2338a5967c9100a4b7c422affb7c496cfac2df9
Roshan Karanth <roshankaranth@gmail.com>
2023-07-04 22:58:39 +0530

    Added heat-map and formatting of files

---
+++ web-server/public/js/temp-heatmap.js
@@ -0,0 +1,40 @@
+testData = {
+    max: 50,
+    data: []
+}
+
+json_airport_1.features.forEach(element => {
+    let data_point = { lat: element.geometry.coordinates[1], lng: element.geometry.coordinates[0], count: element.properties.Temperature }
+    testData.data.push(data_point)
+});
+
+
+
+var cfg = {
+    // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+    // if scaleRadius is false it will be the constant radius used in pixels
+    "radius": 1,
+    "maxOpacity": .8,
+    // scales the radius based on map zoom
+    "scaleRadius": true,
+    // if set to false the heatmap uses the global maximum for colorization
+    // if activated: uses the data maximum within the current map boundaries
+    // (there will always be a red spot with useLocalExtremas true)
+    "useLocalExtrema": true,
+    // which field name in your data represents the latitude - default "lat"
+    latField: 'lat',
+    // which field name in your data represents the longitude - default "lng"
+    lngField: 'lng',
+    // which field name in your data represents the data value - default "value"
+    valueField: 'count'
+};
+
+
+
+var heatmapLayer = new HeatmapOverlay(cfg);
+map.addLayer(heatmapLayer)
+heatmapLayer.setData(testData)
+map.getPanes().overlayPane.style.zIndex = 401
+layerControl.addOverlay(heatmapLayer, "Heat Map")
+console.log(map.getPanes())
+
