1a1e35f35b7ed7b0a1b2b0711e39c148e5a37482
Roshan Karanth <roshankaranth@gmail.com>
2023-07-06 18:53:35 +0530

    Added Humidity Visualization

---
+++ web-server/public/js/humidmap.js
@@ -0,0 +1,28 @@
+var humid_myButton = document.getElementById('humidity')
+var humid_isActive = false
+var humid_layer0 = null
+
+var humid_startEvent = () => {
+    if (!humid_isActive) {
+        humid_myButton.classList.add('clicked')
+
+        var humid_markers = []
+        json_airport_1.features.forEach(element => {
+            var marker = L.circleMarker([element.geometry.coordinates[1], element.geometry.coordinates[0]], { radius: element.properties.Humidity / 5 })
+            humid_markers.push(marker)
+        })
+
+        var Humid_layer = L.layerGroup(humid_markers).addTo(map)
+        map.getPanes().overlayPane.style.zIndex = 401
+        humid_isActive = true
+        humid_layer0 = Humid_layer
+    } else {
+        humid_myButton.classList.remove("clicked")
+        humid_isActive = false
+        humid_layer0.onRemove(map)
+        humid_layer0 = null
+    }
+
+}
+
+humid_myButton.addEventListener('click', humid_startEvent)
--- web-server/public/js/windmap.js
+++ web-server/public/js/windmap.js
@@ -8,15 +8,15 @@ const wind_startEvent = () => {
 
         wind_myButton.classList.add('clicked')
 
-        var markers = []
+        var wind_markers = []
         json_airport_1.features.forEach(element => {
             var icon = L.WindBarb.icon({ lat: element.geometry.coordinates[1], deg: element.properties["Wind Direction"], speed: element.properties["Wind Speed"], pointRadius: 5, strokeLength: 20 });
             var marker = L.marker([element.geometry.coordinates[1], element.geometry.coordinates[0]], { icon: icon })
-            markers.push(marker)
+            wind_markers.push(marker)
         });
 
 
-        var Wind_layer = L.layerGroup(markers).addTo(map)
+        var Wind_layer = L.layerGroup(wind_markers).addTo(map)
         wind_isActive = true
         wind_layer0 = Wind_layer
 
