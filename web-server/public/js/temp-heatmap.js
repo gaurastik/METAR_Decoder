10e82c1da6921efe7e0a8438a09da2b11bd9fdc2
Roshan Karanth <roshankaranth@gmail.com>
2023-07-09 13:43:53 +0530

    Changes temerature range equality

--- web-server/public/js/temp-heatmap.js
+++ web-server/public/js/temp-heatmap.js
@@ -13,14 +13,14 @@ const temp_startEvent = () => {
             if (element.feature.properties.Temperature <= 20) {
                 var icon = L.icon({ iconUrl: './legend/blue.png', iconSize: [10, 10] })
                 element.setIcon(icon)
-            } else if (20 <= element.feature.properties.Temperature && element.feature.properties.Temperature < 25) {
+            } else if (20 < element.feature.properties.Temperature && element.feature.properties.Temperature <= 25) {
                 var icon = L.icon({ iconUrl: './legend/purple.png', iconSize: [10, 10] })
                 element.setIcon(icon)
 
-            } else if (25 <= element.feature.properties.Temperature && element.feature.properties.Temperature < 30) {
+            } else if (25 < element.feature.properties.Temperature && element.feature.properties.Temperature <= 30) {
                 var icon = L.icon({ iconUrl: './legend/megenta.png', iconSize: [10, 10] })
                 element.setIcon(icon)
-            } else if (30 <= element.feature.properties.Temperature && element.feature.properties.Temperature < 35) {
+            } else if (30 < element.feature.properties.Temperature && element.feature.properties.Temperature <= 35) {
                 var icon = L.icon({ iconUrl: './legend/red.png', iconSize: [10, 10] })
                 element.setIcon(icon)
             } else {
